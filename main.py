32bd4889401bdcddeab107c883a4c1237f3450df
Adrian Lee <ajlee4@cps.edu>
2025-02-05 10:59:54 -0600

    ordering

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-import pandas as pd
-import numpy as np
+import csv
+with open("data.csv") as f:
+    data=[tuple(line) for line in csv.reader(f)]
 
-df = pd.read_csv("data.csv", dtype=str)
-print(df)
+def extract_int(str):
+    return int(str[3:],10)
+
+ordered_transactions = []
+
+def order_by_transaction():
+    for i in range (len(data)):
+        if (len(ordered_transactions)) == 0:
+            ordered_transactions.append(data[i])
+        # else:
+        #     for j in range (len(ordered_transactions)):
+        #         if (extract_int(data[i][1])) < (extract_int(ordered_transactions[j][1])):
+        #             ordered_transactions.insert(j,data[i])
+
+order_by_transaction()
+print()
+
 
