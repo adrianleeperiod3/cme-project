cae31610204d0dbc68684fe00b48fd1de053aacc
Adrian Lee <ajlee4@cps.edu>
2025-02-06 10:01:28 -0600

    first commit

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ def order_by_transaction():
     for i in range (len(data)):
         if (len(ordered_transactions)) == 0:
             ordered_transactions.append(data[i])
-        # else:
-        #     for j in range (len(ordered_transactions)):
-        #         if (extract_int(data[i][1])) < (extract_int(ordered_transactions[j][1])):
-        #             ordered_transactions.insert(j,data[i])
+        else:
+            for j in range (len(ordered_transactions)):
+                if (extract_int(data[i][1])) < (extract_int(ordered_transactions[j][1])):
+                    ordered_transactions.insert(j,data[i])
 
 order_by_transaction()
-print()
+print(ordered_transactions)
 
 
