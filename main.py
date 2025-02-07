0b682e7076f5e9a2ccb5c945ed9af7396d99e017
Adrian Lee <ajlee4@cps.edu>
2025-02-06 22:39:53 -0600

    sorted objects

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import csv
+import bisect 
+
 with open("data_copy.csv") as f:
     data=[tuple(line) for line in csv.reader(f)]
-
-def extract_int(str):
-    return int(str[3:],10)
 
 ordered_transactions = []
 
 def order_by_transaction():
+    tempList = []
     for i in range (len(data)):
-        if (len(ordered_transactions)) == 0:
-         ordered_transactions.append(data[i])
-        else:
-            for j in range (len(ordered_transactions)):
-                if (extract_int(data[i][1])) < (extract_int(ordered_transactions[j][1])):
-                    ordered_transactions.append(data[i])
+        bisect.insort(tempList, data[i][1])
+    for t in tempList:
+        for i in range(len(data)):
+            if data[i][1] == t:
+                ordered_transactions.append(data[i])
+        
 
 order_by_transaction()
 print(ordered_transactions)
