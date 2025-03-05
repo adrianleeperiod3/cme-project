dfed0fd90281a52a9f527bffcfdc0f835f45b2f4
Adrian Lee <ajlee4@cps.edu>
2025-03-05 00:14:52 -0600

    sidebar

--- test.py
+++ test.py
@@ -3,10 +3,12 @@ from RandomHistory import create_random_transactions
 
 st.title("Bundling Software")
 
-budget = st.number_input("History Length", min_value=0, step=1)
-risk = st.radio("Preferred Risk Level",["Safe","Risky"])
+st.sidebar.title("User Preferences")
+history_length = st.sidebar.number_input("Number of Simulated Transactions:", min_value=0, step=1)
+risk_level = st.sidebar.selectbox("Trading Risk Level: ",['Safe','Risky','View Both'])
 
-h = create_random_transactions(budget)
-if st.button("Submit"):
-    for i in range(int(budget)):
+
+h = create_random_transactions(history_length)
+if st.sidebar.button("Apply Preferences"):
+    for i in range(int(history_length)):
         st.write(f"Purchase {i+1}: {h[i][2]} on {h[i][4]} \n")
